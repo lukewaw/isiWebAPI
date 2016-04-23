85d652820928dacadbdb0b07c194abe1d63399de
lukewaw <lukas_wawrzyniak@wp.pl>
2016-04-23 15:26:21 +0200

    Some changes

--- StoreWebAPI/App_Start/WebApiConfig.cs
+++ StoreWebAPI/App_Start/WebApiConfig.cs
@@ -20,10 +20,6 @@ namespace StoreWebAPI
                 routeTemplate: "sklepAPI/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-
-
-            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
-            //config.Formatters.XmlFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/xml"));
         }
     }
 }
